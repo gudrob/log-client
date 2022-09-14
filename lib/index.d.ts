7a54769a2bd9fcfa032d5424361e511d5a77f81d
Robert Gudat <robert.gudat@hutter-unger.de>
2022-09-14 14:51:18 +0200

    Updated defintions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default class LogClient {
     private ownAdress;
     loggerAdress: string;
@@ -14,20 +13,3 @@ export default class LogClient {
     Message(message: string): void;
     Log(severity: number | undefined, channel: string, message: string | undefined, data: any): void;
 }
-=======
-export default class LogClient {
-    private ownAdress;
-    loggerAdress: string;
-    onClose: ((logger: LogClient, code: number) => void) | undefined;
-    onError: ((logger: LogClient, error: Error) => void) | undefined;
-    private webSocket;
-    reconnect: boolean;
-    reconnectInterval: number;
-    constructor(ownAdress: string, loggerAdress: string, authString: string, onClose?: ((logger: LogClient, code: number) => void) | undefined, onError?: ((logger: LogClient, error: Error) => void) | undefined);
-    StartMetrics(): void;
-    SendMetrics(): Promise<void>;
-    Start(authString: string): void;
-    Message(message: string): void;
-    Log(severity: number | undefined, message: string, channel: string, data: any): void;
-}
->>>>>>> 00111096e6f7c64fc6c600f8150ca5ca5fdf0329
