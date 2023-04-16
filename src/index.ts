309fa575e8ce956eb01de50932849fbd9950afb8
Robert Gudat <robert.gudat@web.de>
2023-04-16 20:53:31 +0200

    Set websocket to undefined while its closed so sends on closed websocket occur.

--- src/index.ts
+++ src/index.ts
@@ -70,6 +70,8 @@ export default class LogClient {
                 })
 
                 .on('close', (code: number) => {
+                    this.webSocket = undefined;
+
                     this.message('Logger disconnected. Code: ' + code);
 
                     if (this.onClose) return this.onClose(this, code);
